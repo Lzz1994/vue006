c08f067df5909c48601e7019392fb214343bf0fb
张三 <>
2018-01-06 09:27:52 +0800

    添加新闻资讯页面路由

--- src/main.js
+++ src/main.js
@@ -3,16 +3,29 @@ import Vue from "vue"
 
 import VueRouter from "vue-router"
 Vue.use(VueRouter)
-  
+
+
 
 // 2.1 导入 vue-resource
 import VueResource from 'vue-resource'
 // 2.2 安装 vue-resource
 Vue.use(VueResource)
+  // 设置请求的根路径
+Vue.http.options.root = 'http://vue.studyit.io';
+
+
+// 导入格式化时间的插件
+import moment from 'moment'
+// 定义全局的过滤器
+Vue.filter('dateFormat', function (dataStr, pattern = "YYYY-MM-DD HH:mm:ss") {
+  return moment(dataStr).format(pattern)
+})
+
 
 
 //导入mint-ui
 import MintUi from "mint-ui"
+
 
 //从mint-UI中导入头部
 import {Header} from "mint-ui"
@@ -29,6 +42,10 @@ import {Swipe, SwipeItem } from 'mint-ui'
 Vue.component(Swipe.name, Swipe)
 Vue.component(SwipeItem.name, SwipeItem)
 
+//mint-ui导入button
+import { Button } from 'mint-ui';
+
+Vue.component(Button.name, Button);
 
 
 import app from "./App.vue"
--- src/router.js
+++ src/router.js
@@ -5,14 +5,19 @@ import HomeContainer from './components/tabbar/HomeContainer.vue'
 import MemberContainer from './components/tabbar/MemberContainer.vue'
 import ShopcarContainer from './components/tabbar/ShopcarContainer.vue'
 import SearchContainer from './components/tabbar/SearchContainer.vue'
+import newsList from "./components/news/newsList.vue"
+import newsInfo from "./components/news/newsinfo.vue"
 // 3. 创建路由对象
 var router = new VueRouter({
   routes: [
-   { path: '/', redirect: '/home' },
+    { path: '/', redirect: '/home' },
     { path: '/home', component: HomeContainer },
     { path: '/member', component: MemberContainer },
     { path: '/shopcar', component: ShopcarContainer },
-    { path: '/search', component: SearchContainer }
+    { path: '/search', component: SearchContainer },
+    //HomeContainer.vue中的路由  ，/home/
+    {path:"/home/newsList", component:newsList},
+    {path:"/home/newsinfo/:id",component:newsInfo}
   ],
   linkActiveClass: 'mui-active'
 })
